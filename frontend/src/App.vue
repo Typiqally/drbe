519093d818eabc323a1faab984d734f4e90caf89
Jules <juliuszdokrzewski@gmail.com>
2022-06-09 13:05:17 +0200

    Added vuetify, fixed typos, added Document section

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Document />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Document from "./components/Document.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Document,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
