ac937570041b3308c2bb7760f0bb6c7e657698db
Jules <juliuszdokrzewski@gmail.com>
2022-06-15 18:17:32 +0200

    Added success alert

--- frontend/src/components/DocumentComponent.vue
+++ frontend/src/components/DocumentComponent.vue
@@ -1,5 +1,8 @@
 <template fluid>
   <div>
+    <v-alert type="success" v-model="alertFinishedEncryption">
+      Document encrypted
+    </v-alert>
     <v-dialog v-model="dialogSelectionWindowMenu" max-width="500">
       <v-card>
         <v-card-title>Add selected text for encryption</v-card-title>
@@ -32,7 +35,9 @@
         </textarea>
       </v-card-text>
       <v-card-actions>
-        <v-btn color="deep-purple lighten-2"> Finish Encrypting </v-btn>
+        <v-btn color="deep-purple lighten-2" @click="finishEncryption">
+          Finish Encrypting
+        </v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -61,6 +66,7 @@ const textExample = `
 export default {
   // components: { SelectionWindowMenu },
   data: () => ({
+    alertFinishedEncryption: false,
     dialogSelectionWindowMenu: false,
     selectedText: null,
     textSelectedForEncryption: "",
@@ -102,6 +108,9 @@ export default {
       this.currentFragmentDraft = null;
       console.log(roleName);
       this.closeSelectionWindowMenu();
+    },
+    finishEncryption() {
+      this.alertFinishedEncryption = true;
     },
   },
 };
