e6d941a81e00b3d42b850cfbe7d3a1be4df1f95b
Jelle Maas <typiqally@gmail.com>
2022-06-20 18:24:42 +0200

    Add initial `Role` struct

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,4 +1,25 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
 use near_sdk::{env, near_bindgen};
 
 near_sdk::setup_alloc!();
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Role {
+    name: String,
+    owner_account_id: String,
+    public_key: String,
+    encrypted_private_keys: LookupMap<String, String>,
+}
+
+impl Default for Role {
+    fn default() -> Self {
+        Self {
+            name: String::new(),
+            owner_account_id: String::new(),
+            public_key: String::new(),
+            encrypted_private_keys: LookupMap::new(b"r".to_vec()),
+        }
+    }
+}
