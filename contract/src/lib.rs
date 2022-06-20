edad946cee6d3ae01abdd67c6535351b57446e0d
Jelle Maas <typiqally@gmail.com>
2022-06-20 17:58:08 +0200

    Update package manifest and initialize NEAR SDK

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,8 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+
+near_sdk::setup_alloc!();
